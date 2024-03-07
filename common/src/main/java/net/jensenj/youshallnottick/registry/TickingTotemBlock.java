ca2ad6fa8187f70fba9059f23734820a0d1e4e0a
Jensen <jensenbradley@icloud.com>
2024-03-07 01:25:31 +0000

    Ticking Totem Toolip
    Added a tooltip to the ticking totem.

--- common/src/main/java/net/jensenj/youshallnottick/registry/TickingTotemBlock.java
+++ common/src/main/java/net/jensenj/youshallnottick/registry/TickingTotemBlock.java
@@ -1,6 +1,13 @@
 package net.jensenj.youshallnottick.registry;
 
+import net.jensenj.youshallnottick.YouShallNotTick;
+import net.jensenj.youshallnottick.config.ServerConfig;
+import net.minecraft.ChatFormatting;
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
@@ -14,6 +21,8 @@ import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class TickingTotemBlock extends BaseEntityBlock {
     public static final DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
@@ -76,5 +85,19 @@ public class TickingTotemBlock extends BaseEntityBlock {
     @Override
     public BlockEntity newBlockEntity(BlockPos blockPos, BlockState blockState) {
         return new TickingTotemBlockEntity(blockPos, blockState);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack arg, @Nullable BlockGetter arg2, List<Component> list, TooltipFlag arg3) {
+        if(ServerConfig.shouldEnableTotemOfTicking.get()) {
+            list.add(new TranslatableComponent("block." + YouShallNotTick.MOD_ID + ".ticking_totem.info.tooltip").withStyle(ChatFormatting.GRAY));
+            list.add(new TranslatableComponent("block." + YouShallNotTick.MOD_ID + ".ticking_totem.range_h.tooltip")
+                    .append(String.valueOf(ServerConfig.totemMaxEntitySpawnHorizontalDist.get())).withStyle(ChatFormatting.YELLOW));
+            list.add(new TranslatableComponent("block." + YouShallNotTick.MOD_ID + ".ticking_totem.range_v.tooltip")
+                    .append(String.valueOf(ServerConfig.totemMaxEntitySpawnVerticalDist.get())).withStyle(ChatFormatting.YELLOW));
+            list.add(new TranslatableComponent("block." + YouShallNotTick.MOD_ID + ".ticking_totem.redstone.tooltip").withStyle(ChatFormatting.GRAY));
+        }else{
+            list.add(new TranslatableComponent("block." + YouShallNotTick.MOD_ID + ".ticking_totem.disabled.tooltip").withStyle(ChatFormatting.DARK_RED));
+        }
     }
 }
